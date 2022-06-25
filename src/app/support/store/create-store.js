24514f431ece92980b41713ee228190da3339e6c
Nguyen Tuan Linh <linhnt.aim@outlook.com>
2022-06-25 21:45:07 +0700

    store

--- src/app/support/store/create-store.js
+++ src/app/support/store/create-store.js
@@ -1,10 +1,11 @@
+import {registerPropertyFactory, take} from '@/app/support/helpers'
 import Vuex from 'vuex'
 
 export function createStore(options = {}) {
     return {
         installer: Vuex,
         inject: () => ({
-            store: new Vuex.Store(options),
+            store: take(new Vuex.Store(options), store => registerPropertyFactory(store, 'app', store => store._vm)),
         }),
     }
 }
